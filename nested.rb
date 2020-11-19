4eae2414248b47673bd62d579e3d5cfe096b3fd8
John Fuquene <jfuquene@email.wm.edu>
2020-11-19 22:00:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto] <<
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"] }
+
+  programmer_hash[0] = [:yukihiro_matsumoto] => {:known_for => "Ruby", :languages => ["LISP", "C"] }
 end
 
 def changing_alan
